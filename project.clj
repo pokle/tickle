fc5ef14ec885321e5fb72aabd7843149f233b81b
Tushar Pokle <tushar.pokle@gmail.com>
2018-04-25 20:28:53 +1000

    Added midje dep

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "Functional combinators implementation of spec"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [egamble/let-else "1.0.7"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+                   :plugins [[lein-midje "3.2.1"]]}
+
              ;; You can add dependencies that apply to `lein midje` below.
              ;; An example would be changing the logging destination for test runs.
              :midje {}})
