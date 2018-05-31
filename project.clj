c1169e185816f6bf729b924e4286fdd490e25ed5
Tushar Pokle <tushar.pokle@gmail.com>
2018-05-31 23:58:12 +1000

    Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject tickle "0.0.1-SNAPSHOT"
   :description "Functional combinators implementation of spec"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [egamble/let-else "1.0.7"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.1" :exclusions [[org.clojure/clojure]]]]
                    :plugins [[lein-midje "3.2.1"]]}
 
              ;; You can add dependencies that apply to `lein midje` below.
